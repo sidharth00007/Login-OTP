62c5687466532b4da916b621f7be3314961127a2
sidharth00007 <championsid07@gmail.com>
2024-02-03 19:48:41 +0530

    chnges for cors

--- server/server.js
+++ server/server.js
@@ -12,27 +12,37 @@ app.use(cors());
 app.use(morgan('tiny'));
 app.disable('x-powered-by'); // less hackers know about our stack
 
+// Set up CORS options
+const corsOptions = {
+  origin: 'https://login-otp-eight.vercel.app/',
+  methods: 'GET,PUT,POST',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 
 const port = 8080;
 
 /** HTTP GET Request */
 app.get('/', (req, res) => {
-    res.status(201).json("Home GET Request");
+  res.status(201).json("Home GET Request");
 });
 
-
 /** api routes */
-app.use('/api', router)
+app.use('/api', router);
 
-/** start server only when we have valid connection */
-connect().then(() => {
+/** start server only when we have a valid connection */
+connect()
+  .then(() => {
     try {
-        app.listen(port, () => {
-            console.log(`Server connected to http://localhost:${port}`);
-        })
+      app.listen(port, () => {
+        console.log(`Server connected to http://localhost:${port}`);
+      });
     } catch (error) {
-        console.log('Cannot connect to the server')
+      console.log('Cannot connect to the server');
     }
-}).catch(error => {
-    console.log("Invalid database connection...!");
-})
+  })
+  .catch((error) => {
+    console.log('Invalid database connection...!');
+  });
